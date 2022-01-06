b06464232cf34fcf91020f6c4f04cd11c24cf484
Shivani Sharma <shivani1406sharma@gmail.com>
2022-01-06 21:01:58 +0000

    update getallreservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -88,7 +88,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT * FROM reservations WHERE guest_id = $1 and start_date < now() 
+    ORDER BY start_date DESC LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
